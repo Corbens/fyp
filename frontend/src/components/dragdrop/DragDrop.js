370f2c5bf614e2013da03e51bfb1eb6cfd6da50f
Corbens <43511888+Corbens@users.noreply.github.com>
2023-04-09 10:32:21 +0100

    drag & drop responsiveness

--- frontend/src/components/dragdrop/DragDrop.js
+++ frontend/src/components/dragdrop/DragDrop.js
@@ -3,6 +3,7 @@ import { useState, useRef } from 'react'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
+import Grid from '@mui/material/Grid';
 
 import Results from './Results';
 import axios from 'axios'
@@ -10,7 +11,6 @@ import axios from 'axios'
 const DragDrop = ({deck, cards, filler, callback}) => {
     const { user } = useAuthContext()
 
-    // creates an array of length cards where by default the index of a set of cards will have 0 but if a mistake has been made then will be set to 1
     const initializeIncorrectIndexes = () => {
         let arr = []
         for(let i = 0; i < cards; i++){
@@ -51,7 +51,7 @@ const DragDrop = ({deck, cards, filler, callback}) => {
             endGame("Endgame")
         }
 
-        // looks at each element/div inside the English div and updates it if it matches the card that was just dropped onto 
+        // looks at each div inside the static block and updates it if it matches the card that was just dropped onto 
         setStaticBlock((prev) => 
             prev.map((div, i) => {
                 if(String(i) === String(index)){
@@ -62,7 +62,7 @@ const DragDrop = ({deck, cards, filler, callback}) => {
             })
         )
 
-        // looks at each element/div inside the Drop div and updates it if it is the div that was just dropped onto
+        // looks at each div inside the drop block and updates it if it is the div that was just dropped onto
         setDropBlock((prev) =>
             prev.map((div, i) => {
                 if(String(i) === String(index)){
@@ -79,10 +79,10 @@ const DragDrop = ({deck, cards, filler, callback}) => {
             })
         )
 
-        // looks at each element/div inside the Japanese div and updates it if it is the div that was dragged
+        // looks at each div inside the drag block and updates it if it is the div that was dragged
         setDragBlock((prev) => 
             prev.map((div, i) => {
-                let jaIndex = dragBlock[i].props.id.substring(2) // need to get different index from above as Japanese block is in randomized order unlike the English and Drop Blocks
+                let jaIndex = dragBlock[i].props.id.substring(2) // index is in different order as has been shuffled
                 if(String(jaIndex) === String(index)){
                     return <div key={index} className="dragDivs" id={"dg" + index} style={{color: color}}> {deck[index]["ja"]}</div>
                 }else{
@@ -92,14 +92,12 @@ const DragDrop = ({deck, cards, filler, callback}) => {
         )
     }
 
-    // adjusts the properties of each div related to an incorrect drag and drop
     const handleIncorrect = (index) => {
         incorrectRef.current = incorrectRef.current + 1
         let tempIncorrectIndexes = incorrectIndexes.current
         tempIncorrectIndexes[index] = 1
         incorrectIndexes.current = tempIncorrectIndexes
         
-        // looks at each element/div inside the Japanese div and updates if it was the div that was dragged 
         setDragBlock((prev) => 
             prev.map((div, i) => {
                 let jaIndex = dragBlock[i].props.id.substring(2) 
@@ -112,7 +110,7 @@ const DragDrop = ({deck, cards, filler, callback}) => {
         )
     }
 
-    // saves the index of the Japanese div that is being dragged
+    // saves the index of the japanese div that is being dragged
     const onDragStart = (ev, index) => {
         ev.dataTransfer.setData("index", index)
     }
@@ -122,7 +120,6 @@ const DragDrop = ({deck, cards, filler, callback}) => {
         ev.preventDefault()
     }
 
-    // retrieves the index of the dragged div and handle appropriately depending on if it matches the index of the div it was dragged onto or not
     const onDrop = (ev, index) => {
         let draggingId = ev.dataTransfer.getData("index")
         if(String(index) === String(draggingId)){
@@ -142,8 +139,8 @@ const DragDrop = ({deck, cards, filler, callback}) => {
 
     const initializeDrag = () => {
         let ja = deck.map((item, index) => <div key={index} className='dragDivs' id={"dg" + index} onDragStart = {(e) => onDragStart(e, index)} draggable> {item["ja"]} </div>)
-        for(let i = ja.length - 1; i > 0; i--){ // shuffles the order of the japanese cards
-            var j = Math.floor(Math.random() * (i + 1));   
+        for(let i = ja.length - 1; i > 0; i--){
+            var j = Math.floor(Math.random() * (i + 1)) 
             var temp = ja[i];
             ja[i] = ja[j];
             ja[j] = temp;
@@ -155,24 +152,39 @@ const DragDrop = ({deck, cards, filler, callback}) => {
     const [dropBlock, setDropBlock] = useState(() => initializeDrop()) 
     const [dragBlock, setDragBlock] = useState(() => initializeDrag()) 
     const [resultsComponent, setResultsComponent] = useState(null)
-
     const [resultsOpen, setResultsOpen] = useState(false)
+
+
 
     return (
         <div>
             <div className="gameContents">
-                <div className="static">
-                    <h2>English</h2>
-                    {staticBlock}
-                </div>
-                <div className="drop">
-                    <h2>Drop Zone</h2>
-                    {dropBlock}
-                </div>
-                <div className="drag">
-                    <h2>Japanese</h2>
-                    {dragBlock}
-                </div>
+                <Grid container>
+                    <Grid container>
+                        <Grid item xs={4}>
+                            <h2>English</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <h2>Drop Zone</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <h2>Japanese</h2>
+                        </Grid>
+                    </Grid>
+                    {staticBlock.map((value, index) => (
+                    <Grid container>
+                        <Grid item xs={4}>
+                            {staticBlock[index]}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {dropBlock[index]}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {dragBlock[index]}
+                        </Grid>
+                    </Grid>
+                    ))}
+                </Grid>
             </div>
             {resultsComponent &&
                 <div>
